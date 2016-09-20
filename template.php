4d135db18b736fbdcfd3d2b7f307c510dbc50b1a
Florin Buga <florin.buga@gmail.com>
2016-09-20 07:45:56 +0100

    Add Contact Us link before the Essay field

--- template.php
+++ template.php
@@ -3,3 +3,12 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+
+// Add Contact Us link before the Essay field
+function alas_preprocess_field(&$variables, $hook) {
+  $element = $variables['element'];
+  if ($element['#field_name'] == 'field_essay') {
+    $variables['items'][0]['#prefix'] = '<p class="contact"><a href="/contact">' . t('Please contact us for further information') . '</a></p>';
+  }
+}
